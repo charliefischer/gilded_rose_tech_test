648fffc7354add117b5b768281d6f67d69c38c85
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-03-10 16:36:51 +0000

    Refactored backstage pass updater to be DRY

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -26,15 +26,15 @@ class Shop {
   }
 
   #otherItemsUpdater(item) {
-    this.#qualityDepreciator(item)
     item.sellIn -= 1
+    this.#qualityDepreciator(item)
   }
 
   #brieUpdater(item) {
+    item.sellIn -= 1
     if (item.quality < 50) {
       item.quality += 1
     }
-    item.sellIn -= 1
   }
 
   #sulfurasUpdater(item) {
@@ -42,17 +42,12 @@ class Shop {
   }
 
   #backstagePassUpdater(item) {
-    if (item.sellIn <= 0) {
-     return item.quality = 0
-    }
-    if (item.sellIn < 5) {
-      item.quality += 3
-    } else if (item.sellIn > 10) {
-      item.quality += 1
-    } else {
-      item.quality += 2
-    } 
     item.sellIn -= 1
+    if (item.sellIn >= 0) {
+      this.#backstagePassStillValid(item)
+    } else {
+      this.#backstagePassExpired(item)
+    }
   }
 
   #qualityDepreciator(item){
@@ -63,6 +58,20 @@ class Shop {
     } else if (item.quality > 0 && item.sellIn < 0){
       item.quality -= 1
     }
+  }
+
+  #backstagePassExpired(item) {
+      return item.quality = 0
+  }
+
+  #backstagePassStillValid(item) {
+    if (item.sellIn < 5) {
+      item.quality += 3
+    } else if (item.sellIn > 10) {
+      item.quality += 1
+    } else {
+      item.quality += 2
+    } 
   }
 
 
