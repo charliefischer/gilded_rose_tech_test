64ea8895a133beed47212348df87302199619f68
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-03-12 15:03:52 +0000

    Tests for Shop all pass using an item mock

--- test/shop.unit.test.js
+++ test/shop.unit.test.js
@@ -1,5 +1,5 @@
 const Shop = require("../src/gilded_rose");
-
+const AgedBrie = jest.createMockFromModule('../src/agedBrie')
 describe('Shop', () => {
   test('initializes with out any items', () => {
     let shop = new Shop()
@@ -8,9 +8,19 @@ describe('Shop', () => {
 
   test('can store items', () => {
     const mockItems = jest.fn(() => ['Elastic Bands', 'Pencil Sharpener'])
-
     let shop = new Shop(mockItems())
+    
     expect(shop.items).toEqual(mockItems())
+  })
+
+  test('can update items accordingly', () => {
+    mockItem = new AgedBrie()
+    const spy = jest.spyOn(mockItem, 'updateQuality')
+    arr = [mockItem]
+    let shop = new Shop(arr)
+    shop.updateQuality()
+
+    expect(spy).toHaveBeenCalled()
   })
 })
 
